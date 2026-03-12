0ee99facb049c18d6d83863d773aa07ec3c5a89c
mereke.kuanysh <kz9merek@gmail.com>
2026-03-12 15:21:10 +0500

    update Makefile

--- c_src/build_deps.sh
+++ c_src/build_deps.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-# based on build_deps.sh from basho/eleveldb
-
-libiec61850_VSN="master"
+# Set to a stable release version of libiec61850
+libiec61850_VSN="v1.5.3"
 
 set -e
 
@@ -14,12 +13,11 @@ cd $BASEDIR
 
 # detecting gmake and if exists use it
 # if not use make
-# (code from github.com/tuncer/re2/c_src/build_deps.sh
 which gmake 1>/dev/null 2>/dev/null && MAKE=gmake
 MAKE=${MAKE:-make}
 
-# This is a workaround of compilation error of cJSON:
-#   generic selections are a C11-specific feature
+# This is a workaround of compilation error of cJSON: 
+# generic selections are a C11-specific feature
 TARGET_OS=`uname -s`
 if [ "$TARGET_OS" = "Darwin" ] || [ "$TARGET_OS" = "FreeBSD" ]; then
     export CFLAGS="$CFLAGS -Wno-c11-extensions"
@@ -33,8 +31,15 @@ case "$1" in
     build)
         # libiec61850
         if [ ! -d libiec61850 ]; then
-            git clone --depth 1 -b $libiec61850_VSN https://github.com/mz-automation/libiec61850.git libiec61850
+            git clone --depth 1 -b $libiec61850_VSN https://github.com/mz-automation/libiec61850.git
         fi
+        cd libiec61850
+        mkdir -p build
+        cd build
+        
+        # Compile and install locally to _install folder
+        cmake -DCMAKE_INSTALL_PREFIX=_install .. 
+        $MAKE && $MAKE install
         ;;
 esac
 
