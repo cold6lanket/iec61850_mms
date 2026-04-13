4b9973889b892e2f5040c271f5db362f9e1ae977
mereke.kuanysh <kz9merek@gmail.com>
2026-04-13 15:03:16 +0500

    test

--- c_src/client/src/iec61850_mms_client_loop.c
+++ c_src/client/src/iec61850_mms_client_loop.c
@@ -34,8 +34,7 @@ char *start(char *host, int port, const char *password)
         IedConnection_setConnectTimeout(mms_client.connection, 10000);
     }
 
-    LOGDEBUG("host %s", host);
-    LOGDEBUG("port %s", port);
+    fprintf(stdout, "INFO: Connecting to host: %s, port: %d\r\n", host, port);
 
     IedConnection_connect(mms_client.connection, &error, host, port);
 
